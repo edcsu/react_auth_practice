fb8f92bae24ae0b5c3915d76ad4f17ac37553a91
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-21 09:44:32 +0300

    style: change text on submitting

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,11 +1,13 @@
-import { Form, Link, useActionData, useSearchParams } from 'react-router-dom';
+import { Form, Link, useActionData, useNavigation, useSearchParams } from 'react-router-dom';
 
 import classes from './AuthForm.module.css';
 function AuthForm() {
   const data = useActionData()
+  const naviagation = useNavigation()
 
   const [searchParams, setSearchParams] = useSearchParams()
   const isLogin = searchParams.get('mode') === 'login'
+  const isSubmitting = naviagation.state === 'submitting'
 
   return (
     <>
@@ -27,7 +29,7 @@ function AuthForm() {
           <Link to={`?mode=${isLogin ? 'signup' : 'login'}`}>
             {isLogin ? 'Create new user' : 'Login'}
           </Link>
-          <button>Save</button>
+          <button disabled={isSubmitting}>{isSubmitting ? 'Submiting' : 'Save'}</button>
         </div>
       </Form>
     </>
