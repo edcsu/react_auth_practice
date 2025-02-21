c2bc93fb426b2103d6fe9facca888984dc5d33ff
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-21 10:11:34 +0300

    feat: add logout action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 import AuthenticationPage, { action as AuthAction} from './pages/Authentication'
+import { action as LogoutAction} from './pages/Logout'
 const router = createBrowserRouter([
   {
     path: '/',
@@ -63,6 +64,10 @@ const router = createBrowserRouter([
         path: 'auth',
         element: <AuthenticationPage />,
         action: AuthAction
+      },
+      {
+        path: 'logout',
+        action: LogoutAction
       }
     ],
   },
